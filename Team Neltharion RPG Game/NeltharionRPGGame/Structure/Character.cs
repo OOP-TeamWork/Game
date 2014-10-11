e04afa768ef6572b13ba42e4ebfe2b917e1f84bb
b.naskov <boni.mislyashki@gmail.com>
2014-10-11 18:10:24 +0300

    class Character created

---
+++ Team Neltharion RPG Game/NeltharionRPGGame/Structure/Character.cs
@@ -0,0 +1,24 @@
+﻿using NeltharionRPGGame.Events;
+using NeltharionRPGGame.Interfaces;
+using NeltharionRPGGame.Structure;
+
+namespace NeltharionRPGGame.Structure
+{
+    public abstract class Character : Creature, GameObject, IMovable, IRenderable, ICreature
+    {
+        private Weapon[] inventory = new Weapon[3];
+
+        protected Character(int x, int y, int sizeX, int sizeY,
+            int healthPoints, int defensePoints, int attackPoints, int movementSpeed,
+            int attackRange, SpriteType spriteType)
+            : base(x, y, sizeX, sizeY, healthPoints, defensePoints, attackPoints, movementSpeed, attackRange, spriteType)
+        {
+
+        }
+
+        public override void UseWeaponHeld()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
