aef746cb13c0d298c9cf57ae37d15ec4bce28e73
Gabriela Burlakova <gabriela.burlakova@gmail.com>
2014-10-12 09:13:41 +0300

    Updated folder structure

--- Team Neltharion RPG Game/NeltharionRPGGame/Structure/Character.cs
+++ Team Neltharion RPG Game/NeltharionRPGGame/Structure/Character.cs
@@ -1,22 +1,18 @@
-﻿using NeltharionRPGGame.Events;
-using NeltharionRPGGame.Interfaces;
-using NeltharionRPGGame.Structure;
-
-namespace NeltharionRPGGame.Structure
+﻿namespace NeltharionRPGGame.Structure
 {
-    public abstract class Character : Creature, GameObject, IMovable, IRenderable, ICreature
+    public abstract class Character : Creature
     {
         private Weapon[] inventory = new Weapon[3];
 
         protected Character(int x, int y, int sizeX, int sizeY,
-            int healthPoints, int defensePoints, int attackPoints, int movementSpeed,
-            int attackRange, SpriteType spriteType)
-            : base(x, y, sizeX, sizeY, healthPoints, defensePoints, attackPoints, movementSpeed, attackRange, spriteType)
+            SpriteType spriteType, int healthPoints, int defensePoints,
+            int attackPoints, int movementSpeed, int attackRange)
+            : base(x, y, sizeX, sizeY, spriteType, healthPoints,
+            defensePoints, attackPoints, movementSpeed, attackRange)
         {
-
         }
 
-        public override void UseWeaponHeld()
+        public override Weapon UseWeaponHeld()
         {
             throw new System.NotImplementedException();
         }
