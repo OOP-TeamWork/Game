db238454c8cd03c519f67ded38d8277fa2fd29f5
Gabriela Burlakova <gabriela.burlakova@gmail.com>
2014-10-21 16:50:46 +0300

    Fixed a problem in sln

---
+++ NeltharionRPGGame/Structure/Items/Potions/DefencePotion.cs
@@ -0,0 +1,18 @@
+﻿namespace NeltharionRPGGame.Structure.Items.Potions
+{
+    class DefencePotion : Potion
+    {
+        private const int bonusDefenceEffect = 5;
+        public const SpriteType defensePotion = SpriteType.DefensePotion;
+
+        public DefencePotion(int x, int y)
+            : base(x, y, defensePotion)
+        {
+        }
+
+        public override int ReturnBonusEffect()
+        {
+            return bonusDefenceEffect;
+        }
+    }
+}
